4b1b9c9018f95ad4f568aa9ed185065d49f4a6b5
Jeffrey <894860131@qq.com>
2025-06-16 00:29:38 +0800

    4.5-修改启动日志，增加/member，修改logback日志

--- member/src/main/java/com/jeffrey/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jeffrey/train/member/config/MemberApplication.java
@@ -1,15 +1,24 @@
 package com.jeffrey.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.jeffrey.train")
 public class MemberApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Member Application，启动！");
+        LOG.info("测试地址：http://localhost:{}{}/hello", env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
+
     }
 
 }
