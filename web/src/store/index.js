f6880c9093691e2403d1ef379de5273c57f22169
Jeffrey <894860131@qq.com>
2025-06-26 01:36:20 +0800

    6.6-vuex配合h5 session缓存，解决浏览器刷新丢失问题

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,15 +1,18 @@
 import { createStore } from 'vuex'
 
+const MEMBER = "MEMBER";
 export default createStore({
   state: {
-    member: {}
+    // member创建时从sessionStorage中读取，若不存在则为空对象
+    member: window.SessionStorage.get(MEMBER)||{}
   },
   getters: {
   },
   mutations: {
-    setMember(state, member) {
-      state.member = member
-    }
+    setMember(state, _member) {
+      // 设置store内的数据对象，同时刷新sessionStorage内的数据对象
+      state.member=_member
+      window.SessionStorage.set(MEMBER, _member);    }
   },
   actions: {
   },
