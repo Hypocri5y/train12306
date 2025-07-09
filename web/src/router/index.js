cd9e32cecbe8d53041066996a5630b22a58b81aa
Jeffrey <894860131@qq.com>
2025-07-09 16:14:56 +0800

    6.10-增加路由登录拦截，访问所有的控台页面都需要登录

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from "@/views/login";
+import {notification} from "ant-design-vue";
+import store from "@/store";
 
 const routes = [
   {
@@ -8,7 +10,12 @@ const routes = [
     component: Login},
   {
     path: '/',
-    component: () => import('../views/ConsoleView.vue')
+    component: () => import('../views/ConsoleView.vue'),
+    meta: {
+      // 用于指定是否开启路由登录检验拦截
+      // 适用于静态页面，动态页面因为通过Axios拦截器实现动态拦截而不需要指定
+      loginRequire: true
+    }
   }
 ]
 
@@ -17,5 +24,33 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+
+// 路由登录拦截（对于没有请求的静态页面。动态登录拦截通过Axios拦截器实现）
+// 同样采用拦截链实现
+router.beforeEach((to, from, next) => {
+  // 要不要对meta.loginRequire属性做监控拦截
+  if (to.matched.some(function (item) {
+    console.log(item, "是否需要登录校验：", item.meta.loginRequire || false);
+    return item.meta.loginRequire
+  }))
+      // 需要路由登录检验
+  {
+    const _member = store.state.member;
+    console.log("页面登录校验：", _member);
+    if (!_member.token) {
+      console.log("登录失效");
+      notification.error({description: "未登录或登录失效，请重新登录"});
+      next('/login');
+    } else {
+      next();
+    }
+  }
+  // 不需要路由登录检验
+  else {
+    // 拦截链继续处理
+    next();
+  }
+});
 
 export default router
